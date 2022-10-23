df5df35cb56be4eee7637b11584a016a092997a2
leon0399 <hello@leon0399.ru>
2022-10-23 23:24:53 +0300

    feat(Sushiswap): add other networks

--- src/resolvers/sushiswap.ts
+++ src/resolvers/sushiswap.ts
@@ -4,11 +4,30 @@ import retry from 'async-retry'
 import { type Account, AccountType } from '../types/account'
 import { TokenType, type ERC20Token } from '../types/token'
 import type { Resolver } from './resolver'
+import { Network } from '../types/network'
 
 const GROUP = 'sushiswap'
 const STEP = 1000
-const SUBGRAPH_URL =
-  'https://api.thegraph.com/subgraphs/name/sushiswap/exchange'
+
+const NETWORKS: Record<number, string> = {
+  [Network.Mainnet]:
+    'https://api.thegraph.com/subgraphs/name/sushiswap/exchange',
+  [Network.BSC]:
+    'https://api.thegraph.com/subgraphs/name/sushiswap/bsc-exchange',
+  [Network.Polygon]:
+    'https://api.thegraph.com/subgraphs/name/sushiswap/matic-exchange',
+  [Network.Fantom]:
+    'https://api.thegraph.com/subgraphs/name/sushiswap/fantom-exchange',
+  [Network.ArbitrumOne]:
+    'https://api.thegraph.com/subgraphs/name/sushiswap/arbitrum-exchange',
+  [Network.Celo]:
+    'https://api.thegraph.com/subgraphs/name/sushiswap/celo-exchange',
+  [Network.Avalanche]:
+    'https://api.thegraph.com/subgraphs/name/sushiswap/avalanche-exchange',
+  [Network.Moonriver]:
+    'https://api.thegraph.com/subgraphs/name/sushiswap/moonriver-exchange',
+}
+
 const LP_QUERY = gql`
   query LiquidityProviders($first: Int, $skip: Int) {
     pairs(first: $first, skip: $skip) {
@@ -61,10 +80,14 @@ interface Query {
 
 export class SushiSwapResolver implements Resolver {
   getSupportedNetworks(): number[] {
-    return [1]
+    return Object.keys(NETWORKS).map((networkId) => parseInt(networkId))
   }
 
-  async resolve() {
+  async resolve(networkId: number) {
+    // Guaranteed to be not null by `getSupportedNetworks`
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const queryUrl: string = NETWORKS[networkId]!
+
     const accounts: Account[] = DEPLOYMENTS
     const tokens: Record<string, Account> = {}
 
@@ -77,7 +100,7 @@ export class SushiSwapResolver implements Resolver {
     do {
       response = await retry(
         async () =>
-          await request<Query>(SUBGRAPH_URL, LP_QUERY, {
+          await request<Query>(queryUrl, LP_QUERY, {
             first: STEP,
             skip,
           }),
@@ -125,7 +148,7 @@ export class SushiSwapResolver implements Resolver {
       const currentTokensLength = Object.keys(tokens).length
 
       console.log(
-        `Fetched ${GROUP}: ${response.pairs.length} (${currentPoolsLength} pools, ${currentTokensLength} tokens) Offset: ${skip}`
+        `Fetched ${GROUP} (${networkId}): ${response.pairs.length} (${currentPoolsLength} pools, ${currentTokensLength} tokens) Offset: ${skip}`
       )
     } while (response.pairs.length > 0)
 
--- src/types/network.ts
+++ src/types/network.ts
@@ -5,6 +5,8 @@ export enum Network {
   Gnosis = 100,
   Polygon = 137,
   Fantom = 250,
+  Moonriver = 1285,
   ArbitrumOne = 42161,
+  Celo = 42220,
   Avalanche = 43114,
 }
